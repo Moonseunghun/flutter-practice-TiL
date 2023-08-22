c09fdd5a7e0b6aeb5bf4b20c09728a317bb8aaf5
moonseunghun <mangokkaksae2@gmail.com>
2023-08-22 18:07:32 +0900

    ADD : rest api 통신 하는법

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,2 +1,69 @@
 // api C,R,U,D method example
 
+// ex) 예시 통신 방법
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'API Example',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _responseData = "";
+
+  Future<void> fetchData() async {
+    final response = await http
+        .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+
+    if (response.statusCode == 200) {
+      // 서버 응답을 JSON으로 변환
+      Map<String, dynamic> data = json.decode(response.body);
+      setState(() {
+        _responseData = data.toString();
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('API Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: fetchData,
+              child: const Text('Fetch Data'),
+            ),
+            const SizedBox(height: 20),
+            Text(_responseData),
+          ],
+        ),
+      ),
+    );
+  }
+}
