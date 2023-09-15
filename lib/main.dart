fd26e78b6442c5b373be667f999b9e3a1a39765b
moonseunghun <mangokkaksae2@gmail.com>
2023-09-15 15:15:06 +0900

    Chore : 불필요

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BottomNavBar Example',
+      title: 'BottomNavBar',
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
